b7d6489d44228e3abd5719bb85c2d8ca3f1dd0f0
sergencansiz <sergencansiz@gmail.com>
2018-01-17 00:28:44 +0300

    Update fisher_exact_post_hoc.R

--- fisher_exact_post_hoc.R
+++ fisher_exact_post_hoc.R
@@ -1,6 +1,10 @@
 # R
+## Fisher's Exact Test for larger than 2x2 table
+## It can be apply after chi-square test for post hoc
 
 fisher_posthoc <- function(data){
+
+## Rows Combination
 a <- data.frame("v1" = numeric(0) , "v2" = numeric(0))
 for(i in 1:(nrow(data)-1)){
   for(l in 1:(nrow(data)-1)){
@@ -10,7 +14,9 @@ for(i in 1:(nrow(data)-1)){
  }
 }
 a <- a[-which(a$v1 > a$v2),]
-
+  
+  
+## Columns Combination
 b <- data.frame("v1" = numeric(0) , "v2" = numeric(0))
 
     for(i in 1:(ncol(data)-1)){
@@ -20,18 +26,22 @@ b <- data.frame("v1" = numeric(0) , "v2" = numeric(0))
         }
       }
     }
-    b <- b[-which(b$v1 > b$v2),]
 
-    c <- data.frame("v1" = numeric(0) , "v2" = numeric(0) , "v3" = numeric(0) , "v4" = numeric(0))
+b <- b[-which(b$v1 > b$v2),]
+  
+### Combination of all rows and columns 
+c <- data.frame("v1" = numeric(0) , "v2" = numeric(0) , "v3" = numeric(0) , "v4" = numeric(0))
 
-    for(i in 1:nrow(a)){
-      for(l in 1:nrow(b)){
-        if(i != l+1){
-          c <- rbind(c , data.frame("v1" = a[i,1] , "v2" = a[i,2]  , "v3" = b[l,1] , "v4" = b[l,2] ))
-        }
-      }
-
+for(i in 1:nrow(a)){
+  for(l in 1:nrow(b)){
+    if(i != l+1){
+      c <- rbind(c , data.frame("v1" = a[i,1] , "v2" = a[i,2]  , "v3" = b[l,1] , "v4" = b[l,2] ))
     }
+  }
+
+}
+  
+### FISHER TEST FOR ALL 2x2 TABLES
     result <- data.frame("category_row1" = character(0),
                          "category_row2" = character(0),
                          "category_col1" = character(0),
